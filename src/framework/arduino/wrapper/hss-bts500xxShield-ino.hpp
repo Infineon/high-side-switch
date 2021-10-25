2bc0323febc829f028322b77c05e7ff61ccf6571
Panday Swarnam (IFIN DES SDF SCS EPE) <Swarnam.Panday@infineon.com>
2021-10-25 15:58:20 +0530

    adding shield config hpp

---
+++ src/framework/arduino/wrapper/hss-bts500xxShield-ino.hpp
@@ -0,0 +1,23 @@
+/**
+ * @file        hss-bts500xxShield.hpp
+ * @brief       Class for BTS500xx-1EPP (12V) Shield
+ * @copyright   Copyright (c) 2021 Infineon Technologies AG
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include "hss-arduino.hpp"
+#include "hss-bts500xxShield.hpp"
+#include "variants.hpp"
+#include "hss-shield-config.hpp"
+
+class Bts500xxShieldIno : Bts500xxShield
+{
+    public:
+                        Bts500xxShieldIno(BtsVariants_t *variantSel);
+                        Bts500xxShieldIno(BtsVariants_t *variantSel, BTShardwareconfig_t config);
+                        ~Bts500xxShieldIno();
+        Error_t         init_shield();
+        Error_t         deinit_shield();
+
+};
